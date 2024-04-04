b8cf28ff6175089c36a1641a39c6485bead48ae6
TwizzyBomb <1740367+TwizzyBomb@users.noreply.github.com>
2024-04-03 22:47:26 -0400

    original commit

---
+++ src/main/java/hesh/zone/fincat/FinCatApplication.java
@@ -0,0 +1,13 @@
+package hesh.zone.fincat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinCatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinCatApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hesh/zone/fincat/FinCatApplicationTests.java
@@ -0,0 +1,13 @@
+package hesh.zone.fincat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinCatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
