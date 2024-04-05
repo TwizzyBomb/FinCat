60e9a5aa66ce3077d72c49411de4ff80f07c85e5
TwizzyBomb <1740367+TwizzyBomb@users.noreply.github.com>
2024-04-04 21:53:39 -0400

    [FINCAT-10] got application.yml properties working

--- src/main/java/hesh/zone/fincat/FinCatApplication.java
+++ src/main/java/hesh/zone/fincat/FinCatApplication.java
@@ -1,13 +1,28 @@
 package hesh.zone.fincat;
 
+import hesh.zone.fincat.config.PathProperties;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class FinCatApplication {
+public class FinCatApplication implements CommandLineRunner {
+	@Autowired
+	private PathProperties paths;
 
 	public static void main(String[] args) {
 		SpringApplication.run(FinCatApplication.class, args);
+
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println("getChargeList: " + paths.getChargeList());
+		System.out.println("getIncomeList: " + paths.getIncomeList());
+		System.out.println("getTestFileSmall: " + paths.getTestFileSmall());
+		System.out.println("getTestFile: " + paths.getTestFile());
+		System.out.println("getPipeFile: " + paths.getPipeFile());
+
+	}
 }
