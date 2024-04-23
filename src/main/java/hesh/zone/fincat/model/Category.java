ab18674276577c335ff910e99e0e4e4f2253e6f9
TwizzyBomb <1740367+TwizzyBomb@users.noreply.github.com>
2024-04-23 18:28:19 -0400

    [FINCAT-8]Added sub categories functionality, updated createBreakdown method to include them

--- src/main/java/hesh/zone/fincat/model/Category.java
+++ src/main/java/hesh/zone/fincat/model/Category.java
@@ -4,51 +4,97 @@ import java.util.HashMap;
 import java.util.List;
 
 public class Category {
-    private String name;
-    private double total;
-    private HashMap<String, Charge> charges;
-    private Category parentCategory;
-    private List<Category> subCategories;
-    public Category(String name) {
-        total = 0.0;
-        charges = new HashMap<String, Charge>();
-    }
-
-    public void add(Charge charge){
-        addToTotal( charge.getTotal() );
-        StringBuilder sb = new StringBuilder();
-        sb.append(charge.getCategory());
-        sb.append(charge.getTransactionDate().replace("/", ""));
-        sb.append(charge.getAmount());
-        String chargeKey = sb.toString();
-        charges.put(chargeKey, charge);
-    }
-
-    public void addToTotal(double price){
-        this.total += price;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public double getTotal() {
-        return total;
-    }
-
-    public void setTotal(double total) {
-        this.total = total;
-    }
-
-    public HashMap<String, Charge> getCharges() {
-        return charges;
-    }
-
-    public void setCharges(HashMap<String, Charge> charges) {
-        this.charges = charges;
-    }
+  private String name;
+  private double total;
+  private HashMap<String, Charge> charges;
+  private String parentCatName;
+  private HashMap<String, Category> childCategories;
+  
+  /**
+   * Category - regular constructor
+   * @param name - name of the category
+   */
+  public Category(String name) {
+    this.name = name;
+    total = 0.0;
+    charges = new HashMap<String, Charge>();
+    childCategories = new HashMap<>();
+  }
+  
+  /**
+   * Category - child category constructor
+   * @param name - name of this child category
+   * @param parentName - name of the parent category associated with this category
+   */
+  public Category(String name, String parentName) {
+    this.name = name;
+    total = 0.0;
+    charges = new HashMap<String, Charge>();
+    this.parentCatName = parentName;
+    childCategories = new HashMap<>();
+  }
+  
+  /**
+   * add - Takes a charge, concats it's category, date and amount into a key
+   * puts it in the categories charge map
+   *
+   * @param charge
+   */
+  public void add(Charge charge) {
+    //addToTotal(charge.getTotal());
+    StringBuilder sb = new StringBuilder();
+    sb.append(charge.getCategory());
+    sb.append(charge.getTransactionDate().replace("/", ""));
+    sb.append(charge.getAmount());
+    String chargeKey = sb.toString();
+    charges.put(chargeKey, charge);
+  }
+  
+  public void addCategory(Category category) {
+    this.childCategories.put(category.getName(), category);
+  }
+  
+  public void addToTotal(double price) {
+    this.total += price;
+  }
+  
+  public String getName() {
+    return name;
+  }
+  
+  public void setName(String name) {
+    this.name = name;
+  }
+  
+  public double getTotal() {
+    return total;
+  }
+  
+  public void setTotal(double total) {
+    this.total = total;
+  }
+  
+  public HashMap<String, Charge> getCharges() {
+    return charges;
+  }
+  
+  public void setCharges(HashMap<String, Charge> charges) {
+    this.charges = charges;
+  }
+  
+  public String getParentCatName() {
+    return parentCatName;
+  }
+  
+  public void setParentCatName(String parentName) {
+    this.parentCatName = parentName;
+  }
+  
+  public HashMap<String, Category> getChildCategories() {
+    return childCategories;
+  }
+  
+  public void setChildCategories(HashMap<String, Category> childCategories) {
+    this.childCategories = childCategories;
+  }
 }
