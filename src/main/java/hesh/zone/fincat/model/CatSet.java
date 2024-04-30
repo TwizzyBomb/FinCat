0846a031b6f0ee989ac8cc61f77f42eb7a35a10b
TwizzyBomb <1740367+TwizzyBomb@users.noreply.github.com>
2024-04-29 22:37:43 -0400

    [FINCAT-8]Added sub categories functionality, updated createBreakdown method to include them (#2)

--- src/main/java/hesh/zone/fincat/model/CatSet.java
+++ src/main/java/hesh/zone/fincat/model/CatSet.java
@@ -2,68 +2,149 @@ package hesh.zone.fincat.model;
 
 import java.text.DecimalFormat;
 import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Categories - This is a list which will keep a running total for finding
  * the proportions the category takes up of the total
  */
 public class CatSet {
-//    private static CatSet instance;
-    private HashMap<String, Category> categoriesMap;
-    private double runningTotal;
-    public CatSet(){
-        categoriesMap = new HashMap<String, Category>();
-        runningTotal = 0.0;
-    }
-
-    // not what we want because we need an income and charge list instance to exist simultaniously
+  //    private static CatSet instance;
+  private HashMap<String, Category> categoriesMap;
+  private double runningTotal;
+  
+  public CatSet() {
+    categoriesMap = new HashMap<String, Category>();
+    runningTotal = 0.0;
+  }
+  
+  // not what we want because we need an income and charge list instance to exist simultaniously
 //    public static CatSet getInstance() {
 //        if (instance == null) {
 //            instance = new CatSet();
 //        }
 //        return instance;
 //    }
-
-    public void add(String name, Charge charge){
-        // add to running total:
-        runningTotal += charge.getAmount();
-
-        // check whether category existed or not
-        if(!categoriesMap.containsKey(name)){
-            // add new key and category
-            Category cat = new Category(name);
-            cat.addToTotal(charge.getAmount());
-            categoriesMap.put(name, cat);
-
+  
+  public void add(String rawCatName, Charge charge) {
+    String parentCatName = null;
+    String childCatName = null;
+    if (rawCatName.contains(":")) {
+      //split the parent from sub-category
+      int pos = rawCatName.indexOf(":");
+      parentCatName = rawCatName.substring(0, pos);
+      childCatName = rawCatName.substring(pos + 1, rawCatName.length());
+      System.out.println("Child Name:" + childCatName + " Parent name:" + parentCatName);
+    } else {
+      parentCatName = rawCatName;
+    }
+    
+    // does parent exist?
+    if (!categoriesMap.containsKey(parentCatName)) {
+      
+      // parent doesn't exist, create one
+      Category parentCat = new Category(parentCatName);
+      parentCat.addToTotal(charge.getAmount());
+      
+      // is there a child to add as well?
+      if (null != childCatName) {
+        // add charges to child category, and add to parent
+        Category childCat = new Category(childCatName);
+        childCat.add(charge);
+        childCat.addToTotal(charge.getAmount());
+        childCat.setParentCatName(parentCatName);
+        parentCat.getChildCategories().put(childCatName, childCat);
+      }
+      
+      // add parent
+      categoriesMap.put(parentCatName, parentCat);
+      
+    } else {
+      // parent exits, does child?
+      if (null != childCatName) {
+        
+        // child in parent cat set?
+        if (null == categoriesMap.get(parentCatName).getChildCategories().get(childCatName)) {
+          // child doesn't exist
+          Category childCat = new Category(childCatName);
+          childCat.addToTotal(charge.getAmount());
+          childCat.setParentCatName(parentCatName);
+          childCat.add(charge);
+          categoriesMap.get(parentCatName).getChildCategories().put(childCatName, childCat);
+          categoriesMap.get(parentCatName).addToTotal(charge.getAmount());
         } else {
-            // add the charge to the existing category object
-            categoriesMap.get(name).add(charge);
+          // both already exist - add the charge to the child cat
+          categoriesMap.get(parentCatName).getChildCategories().get(childCatName).add(charge);
+          categoriesMap.get(parentCatName).addToTotal(charge.getAmount());
         }
+      } else {
+        // just add the charge to the existing category object
+        categoriesMap.get(parentCatName).add(charge);
+        categoriesMap.get(parentCatName).addToTotal(charge.getAmount());
+      }
     }
+    
+    // add to running total:
+    runningTotal += charge.getAmount();
 
-    public void printTotals() {
-        int n = categoriesMap.size();
-        DecimalFormat df = new DecimalFormat("0.00");
+//    // check whether solo category existed or not
+//    if (!categoriesMap.containsKey(rawCatName)) {
+//      // add new key and category
+//      Category cat = new Category(rawCatName);
+//      cat.addToTotal(charge.getAmount());
+//      categoriesMap.put(parentCatName, cat);
+//
+//    } else {
+//      // add the charge to the existing category object
+//      categoriesMap.get(parentCatName).add(charge);
+//    }
+  }
 
-        if (n > 0){
-            // loop through, display totals and percentages
-            System.out.println("Breakdown:\n");
-            for(String catName: categoriesMap.keySet() ) {
-                System.out.println(df.format(categoriesMap.get(catName).getTotal())
-                        + " towards "
-                        + catName
-                        + "\t"
-                        + df.format((categoriesMap.get(catName).getTotal() / runningTotal) * 100)
-                        + "% of total spent this month");
-            }
+//  public void add(String name, Charge charge) {
+//    // add to running total:
+//    runningTotal += charge.getAmount();
+//
+//    // check whether category existed or not
+//    if (!categoriesMap.containsKey(name)) {
+//      // add new key and category
+//      Category cat = new Category(name);
+//      cat.addToTotal(charge.getAmount());
+//      categoriesMap.put(name, cat);
+//
+//    } else {
+//      // add the charge to the existing category object
+//      categoriesMap.get(name).add(charge);
+//    }
+//  }
+  
+  public void createBreakdown() {
+    int n = categoriesMap.size();
+    DecimalFormat df = new DecimalFormat("0.00");
+    
+    if (n > 0) {
+      // loop through, display totals and percentages
+      System.out.println("Breakdown:\n");
+      for (Category parentCat : categoriesMap.values() ) {
+        double totalPercent = 0.0;
+        for( Category childCat : parentCat.getChildCategories().values() ) {
+          totalPercent += childCat.getTotal();
+          System.out.println(df.format(childCat.getTotal())
+                  + " towards " + childCat.getName() + "\t\t"
+                  + df.format((childCat.getTotal() / runningTotal) * 100)
+                  + "% of total spent this month");
         }
+        System.out.println("total percent for " + parentCat.getName() + ": "
+                + df.format((totalPercent / runningTotal) * 100) + "%");
+        
+      }
     }
-
-    public HashMap<String, Category> getCategoriesMap() {
-        return categoriesMap;
-    }
-
-    public void setCategoriesMap(HashMap<String, Category> categoriesMap) {
-        this.categoriesMap = categoriesMap;
-    }
+  }
+  
+  public HashMap<String, Category> getCategoriesMap() {
+    return categoriesMap;
+  }
+  
+  public void setCategoriesMap(HashMap<String, Category> categoriesMap) {
+    this.categoriesMap = categoriesMap;
+  }
 }
--- src/main/java/hesh/zone/fincat/service/Cmd.java
+++ src/main/java/hesh/zone/fincat/service/Cmd.java
@@ -11,70 +11,86 @@ import java.util.List;
 import java.util.Scanner;
 
 public class Cmd {
-    private static CatSet chargeList = null;
-    private static CatSet incomeList = null;
-
-    public void terminalRun() throws FileNotFoundException, IOException {
-
-        FileSystem fileSystem = new FileSystem();
-        chargeList = fileSystem.getCatSetFile(Constants.CHARGE_LIST_PATH);
-        incomeList = fileSystem.getCatSetFile(Constants.INCOME_LIST_PATH);
-
-        // load old data into charge & income list objects and fetch new transactions into data List
-        List<String[]> data = fileSystem.loadTransactionFile();
-
-        try( Scanner scanner = new Scanner(System.in) ){
-            // hi
-            System.out.println("Hi");
-
-            // no data means say goodbye!
-            if(data.size()==0){
-                System.out.println("No data found, thanks for playing!");
-                exit();
-            }
-
-            // Process the new charge data as needed
-            for (String[] row : data) {
-                // get category names from user
-                System.out.println("How would you like to categorize:" + row[4]);
-                String categoryName = scanner.nextLine();
-                Charge charge = new Charge(
-                        row[0].replace("\"", ""),
-                        "",
-                        row[4].replace("\"", ""),
-                        Math.abs(Double.parseDouble(row[1].replace("\"", ""))),
-                        categoryName);
-                System.out.println("\nAdded " + charge.getDescription() +
-                        " to " + charge.getCategory() +
-                        " with amount " + charge.getAmount());
-
-                // check if income or charge and add to appropriate obj
-                if(row[1].replace("\"", "").startsWith("-")){
-                    // add charge to category in list, create category if doesn't exist
-                    chargeList.add(categoryName, charge);
-                    System.out.println("added to charges");
-                } else {
-                    // positive = income
-                    incomeList.add(categoryName, charge);
-                    System.out.println("added to income");
-                }
-
-                System.out.println(); // Move to the next line for the next row
-            }
-
-            // serialize current sorted category objects to json
-            fileSystem.writeJson(Constants.CHARGE_LIST_PATH, chargeList);
-            fileSystem.writeJson(Constants.INCOME_LIST_PATH, incomeList);
-
+  private static CatSet chargeList = null;
+  private static CatSet incomeList = null;
+  
+  public void terminalRun() throws FileNotFoundException, IOException {
+    
+    FileSystem fileSystem = new FileSystem();
+    chargeList = fileSystem.getCatSetFile(Constants.CHARGE_LIST_PATH);
+    incomeList = fileSystem.getCatSetFile(Constants.INCOME_LIST_PATH);
+    
+    // load old data into charge & income list objects and fetch new transactions into data List
+    List<String[]> data = fileSystem.loadTransactionFile();
+    
+    try (Scanner scanner = new Scanner(System.in)) {
+      // hi
+      System.out.println("Hi");
+      
+      // no data means say goodbye!
+      if (data.size() == 0) {
+        System.out.println("No data found, thanks for playing!");
+        if( null != chargeList ){
+          chargeList.createBreakdown();
         }
-
-        // print totals
-        chargeList.printTotals();
-        incomeList.printTotals();
+        if( null != incomeList ){
+          incomeList.createBreakdown();
+        }
+        // bye!
+        exit();
+      }
+      
+      // Process the new charge data as needed
+      for (String[] row : data) {
+        // get category names from user
+        System.out.println("How would you like to categorize:" + row[4]);
+        String categoryName = scanner.nextLine();
+        String chargeName = categoryName;
+        
+        // parent category - either add to existing or create parent and child
+        if (categoryName.contains(":")) {
+          //split the parent from sub-category
+          int pos = categoryName.indexOf(":");
+          chargeName = categoryName.substring(pos + 1, categoryName.length());
+        }
+        // create charge
+        Charge charge = new Charge(
+                row[0].replace("\"", ""),
+                "",
+                row[4].replace("\"", ""),
+                Math.abs(Double.parseDouble(row[1].replace("\"", ""))),
+                chargeName);
+        // log
+        System.out.println("\nCreated cat" + charge.getDescription() + " to " + charge.getCategory() + " with amount " + charge.getAmount());
+        
+        // delegate all the sorting to the CatSet class - check if income or charge and add to appropriate obj
+        if (row[1].replace("\"", "").startsWith("-")) {
+          // add charge to category in list, create category if doesn't exist
+          chargeList.add(categoryName, charge);
+          System.out.println("added to charges");
+        } else {
+          // positive = income
+          incomeList.add(categoryName, charge);
+          System.out.println("added to income");
+        }
+        
+        System.out.println(); // Move to the next line for the next row
+      }
+      
+      // serialize current sorted category objects to json
+      fileSystem.writeJson(Constants.CHARGE_LIST_PATH, chargeList);
+      fileSystem.writeJson(Constants.INCOME_LIST_PATH, incomeList);
     }
-
-    private void exit(){
-        // free up resources and attempt to exit
-        System.exit(0);
-    }
+    
+    // print totals
+    chargeList.createBreakdown();
+    incomeList.createBreakdown();
+    
+    exit();
+  }
+  
+  private void exit() {
+    // free up resources and attempt to exit
+    System.exit(0);
+  }
 }
